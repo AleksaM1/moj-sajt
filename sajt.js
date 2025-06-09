5871e3b3872e27969125a2b1803045b42d08e904
AleksaM1 <aleksamelentijevic@tehnickacacak.edu.rs>
2025-06-09 13:49:41 +0200

    Add files via upload

--- sajt.js
+++ sajt.js
@@ -23,3 +23,19 @@ function provera(email) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 }
+function Posalji(){
+    var Slikovi=document.getElementById("S-tier").value;
+    var Alikovi=document.getElementById("A-tier").value;
+    var Blikovi=document.getElementById("B-tier").value;
+    var Clikovi=document.getElementById("C-tier").value;
+
+    document.getElementById("S-tierIspis").innerText=Slikovi;
+    document.getElementById("A-tierIspis").innerText=Alikovi;
+    document.getElementById("B-tierIspis").innerText=Blikovi;
+    document.getElementById("C-tierIspis").innerText=Clikovi;
+
+    document.getElementById("S-tier").value="";
+    document.getElementById("A-tier").value="";
+    document.getElementById("B-tier").value="";
+    document.getElementById("C-tier").value="";
+}
